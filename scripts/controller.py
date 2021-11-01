ec82db86df3038299a9bb19a3d1c65d7fffddefb
yagmur <aktas.yagmur@gmail.com>
2021-11-01 21:30:49 +0100

    the robot finally stops when it reachs the target with signal_shutdown
    (reachs the target very by chance not with a good way )

--- scripts/controller.py
+++ scripts/controller.py
@@ -3,54 +3,79 @@ from vs_project.msg import MarkerPose
 from vs_project.msg import detectedMarker
 import numpy as np
 from geometry_msgs.msg import Twist
+import sys
 
 ROBOT_MARKER_ID = 0
 TARGET_MARKER_ID = 3  
 target_assigned = False
 robot_assigned = False
+target_reached = False
+
 
 Z = 1 # dont know what to put now
 
 uv_robot = []
 uv_target = []
 interaction_matrix = []
+error = []
 
-def controller(robot, target, alpha):
-    pixel_velocity = []
+def controller(robot, target, alpha=0.5):
     global interaction_matrix
+    global error
+    global target_reached
     interaction_matrix.clear()
+    error.clear()
    
     for i in range(3):  # using only 3 corners not 4
-        pixel_velocity.append(robot[i][0] - target[i][0])
-        pixel_velocity.append(robot[i][1] - target[i][1])
+        error.append(robot[i][0] - target[i][0])
+        error.append(robot[i][1] - target[i][1])
 
     calculate_interaction_matrix(robot)
    
-    pixel_velocity_np = np.array(pixel_velocity).reshape(6,1)
+    error_np = np.array(error).reshape(6,1)
     interaction_matrix_np = np.array(interaction_matrix).reshape(6,6)
 
     interaction_matrix_np = np.linalg.inv(interaction_matrix_np) #inverse of the interaction matrix
 
-    print("pixel velocity",pixel_velocity_np)
+    print("error",error_np)
+
+    error_norm = np.linalg.norm(error_np)
+
+    robot_vel = Twist()
+
+    if error_norm < 100:
+        robot_vel.linear.x = 0
+        #robot_vel.linear.y = robot_velocity[1][0]
+        #robot_vel.linear.z = robot_velocity[2][0]
+
+        #robot_vel.angular.x = robot_velocity[3][0]
+        #robot_vel.angular.y = robot_velocity[4][0]
+        robot_vel.angular.z = 0
+        
+        pub.publish(robot_vel)
+        print("robot reached th target ciaou")
+        rospy.signal_shutdown("robot reached th target ciaou")
+        
+
+
+    print("error norm", error_norm)
     print("interaction matrix",interaction_matrix_np)
 
     
-    robot_velocity = -alpha * np.matmul(interaction_matrix_np, pixel_velocity_np)
+    robot_velocity = alpha * np.matmul(interaction_matrix_np, error_np)
 
     print("ROBOT VELOCITY", robot_velocity) 
 
-    robot_vel = Twist()
     robot_vel.linear.x = robot_velocity[0][0]
-    print("LINEAR X",robot_vel.linear.x)
-    robot_vel.linear.y = robot_velocity[1][0]
-    print("LINEAR Y",robot_vel.linear.y)
-    robot_vel.linear.z = robot_velocity[2][0]
+    #robot_vel.linear.y = robot_velocity[1][0]
+    #robot_vel.linear.z = robot_velocity[2][0]
 
-    robot_vel.angular.x = robot_velocity[3][0]
-    robot_vel.angular.y = robot_velocity[4][0]
+    #robot_vel.angular.x = robot_velocity[3][0]
+    #robot_vel.angular.y = robot_velocity[4][0]
     robot_vel.angular.z = robot_velocity[5][0]
 
     pub.publish(robot_vel)
+
 
 
 def calculate_interaction_matrix(robot):
@@ -60,36 +85,42 @@ def calculate_interaction_matrix(robot):
   
 
 def callback(msg):   
+    print("hello")
     global target_assigned
-    global robot_assigned
+    global robot_assigned   
 
     if msg.id == ROBOT_MARKER_ID:
      #   print("robot uv") 
+        uv_robot.clear()
         uv_robot.append((msg.corner1.x, msg.corner1.y))
         uv_robot.append((msg.corner2.x, msg.corner2.y))
         uv_robot.append((msg.corner3.x, msg.corner3.y))
         robot_assigned = True
       #  uv_robot.append((msg.corner4.x, msg.corner4.y))
     elif msg.id == TARGET_MARKER_ID:  # no need to assign target possition again and again
-        if not target_assigned:
-         #   print("target uv") 
-            uv_target.append((msg.corner1.x, msg.corner1.y))
-            uv_target.append((msg.corner2.x, msg.corner2.y))
-            uv_target.append((msg.corner3.x, msg.corner3.y))
-        #  uv_target.append((msg.corner4.x, msg.corner4.y))
-            target_assigned = True
+        #   print("target uv") 
+        uv_target.clear()
+        uv_target.append((msg.corner1.x, msg.corner1.y))
+        uv_target.append((msg.corner2.x, msg.corner2.y))
+        uv_target.append((msg.corner3.x, msg.corner3.y))
+    #  uv_target.append((msg.corner4.x, msg.corner4.y))
+        target_assigned = True
     else:
         raise ValueError("check your marker IDs")    
 
     if target_assigned and robot_assigned:
-        controller(uv_robot, uv_target, 1)
-
-
+        controller(uv_robot, uv_target)
+        robot_assigned = False # we should be sure if the new robot position came before calling controller serially
+        target_assigned = False
+        
 
 
 rospy.init_node('controller')
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+rate = rospy.Rate(10)
 
-while not rospy.is_shutdown(): 
-  sub = rospy.Subscriber('/detected_marker', detectedMarker, callback)
-  rospy.spin()
+
+sub = rospy.Subscriber('/detected_marker', detectedMarker, callback)
+rospy.spin()
+
+  
